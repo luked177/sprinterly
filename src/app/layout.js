539547bbee436055f24e1a7535147f8a3c625b80
luked177 <43680876+luked177@users.noreply.github.com>
2023-05-17 21:32:11 +0100

    Client 2

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,3 @@
-"use client";
-
 import { Suspense } from "react";
 import "./globals.css";
 import { Inter } from "next/font/google";
@@ -17,7 +15,7 @@ export default function RootLayout({ children }) {
 		<html lang='en'>
 			<body className={inter.className}>
 				<Header />
-				<Suspense fallback={<p>Loading...</p>}>{children}</Suspense>
+				{children}
 			</body>
 		</html>
 	);
